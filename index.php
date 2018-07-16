2f028782fbfd17e42c9cf50c77d9c10f14298ec7
Paweł Porosło <pawel.poroslo@gmail.com>
2018-07-16 13:38:44 +0200

    Procesor

--- index.php
+++ index.php
@@ -1,29 +1,46 @@
-<?php
-    echo "<h1>Cwiczenia z GITa</h1>";
-?>
-<div>
-    <ol>
-        <li>Zmodyfikować plik <i>index.php</i> dodając nagłówek <code>h3</code>
-            z bieżącą datą (sprawdzić jak pobrac bieżącą datę w PHP)</li>
-        <li>Wykonać komendę* <code>git status</code></li>
-        <li>Wykonać komendę* <code>git add --all</code></li>
-        <li>WYkonać komendę* <code>git commit -am "Twój komentarz"</code></li>
-        <li>Wykonać komendę* <code>git branch</code> </li>
-        <li>WYkonać komendę* <code>git checkout -b poboczna_galaz</code></li>
-        <li>Wykonać komendę <code>git status</code></li>
-        <li>Zmodyfikować plik <i>index.php</i> zmienić color tła na <i>#ccc</i></li>
-        <li>Wykonać komendy <code>git add --all</code> i <code>git commit -am</code></li>
-        <li>Wykonać komendę <code>git checkout master</code> co się stało? ;)</li>
-        <li>Wykonać komendę <code>git log</code></li>
-        <li>Poczytać o komendzie <code>git pull</code> i <code>git push</code></li>
-        <li>Poczytać o komendzie <code>git revert</code></li>
-    </ol>
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <title>Procesor</title>
+ </head>
+ <body>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+  <nav class="navbar navbar-dark bg-light">
+   <a class="navbar-brand">Menu</a>
+   <button class="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    <form class="form-inline">
+     <input class="form-control mr-sm-2" type="search" placeholder="Szukaj" aria-label="Search">
+     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Szukaj</button>
+    </form>
+   </nav>
+   <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.YD0cHz0eoEwtrWLMlyrXQwHaD6%26pid%3D15.1&f=1" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">AMD</h5>
+    <p class="card-text"> RYZEN THREADRIPPER 1950x</p>
+	 <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cena: 800$</li>
+  </ul>
+	 <p class="card-text">Parametry techniczne:</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Rdzenie: 16</li>
+    <li class="list-group-item">Wątki: 32</li>
+    <li class="list-group-item">Częstotliwość bazowa:3.4Ghz <br> Częstotliwość turbo:4Ghz</li>
+	<li class="list-group-item">CMOS: 14nm</li>
+	<li class="list-group-item">Pamięć Cache L1: 1.5MB<br>Pamięć Cache L2: 8MB<br>Pamięć Cache L3: 32MB</li>
+	<li class="list-group-item">Domyślne TDP: 180W</li>
+  </ul>
+  <div class="card-body">
+    <a href="https://www.amd.com/en/products/cpu/amd-ryzen-threadripper-1950x" class="card-link">Link do strony AMD</a>
+  </div>
 </div>
-
-<h2 style="color: red">
-    * - Najpierw poczytaj o tej komendzie
-</h2>
-<html>
-<body bgcolor=#ccc>
-</body>
-</html>
+  </body>
+ </html>
