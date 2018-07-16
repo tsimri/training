0a2c59545b9667f8ec8e571428e21c59360ad784
Paweł Porosło <pawel.poroslo@gmail.com>
2018-07-16 15:51:02 +0200

    Wygląd

--- index.php
+++ index.php
@@ -10,37 +10,75 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
-  <nav class="navbar navbar-dark bg-light">
-   <a class="navbar-brand">Menu</a>
-   <button class="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <nav class="navbar navbar-dark bg-light"> 
+
+   <a class="navbar-brand">Menu</a> 
+   
     <form class="form-inline">
      <input class="form-control mr-sm-2" type="search" placeholder="Szukaj" aria-label="Search">
      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Szukaj</button>
     </form>
+	
+	<button class="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
    </nav>
-   <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.YD0cHz0eoEwtrWLMlyrXQwHaD6%26pid%3D15.1&f=1" alt="Card image cap">
+ 
+   <div class="card" style="width: 22rem;">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+   <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+  </ol>
+   <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.wccftech.com%2Fwp-content%2Fuploads%2F2017%2F06%2FAMD-Ryzen-Threadripper_2.png&f=1" alt="1">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fimages10.newegg.com%2FNeweggImage%2FProductImage%2F19-113-447-V01.jpg&f=1" alt="2">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.LPkx5CbBzW8-BVfKzK0ILAHaFH%26pid%3D15.1&f=1" alt="3">
+    </div>
+	 <div class="carousel-item">
+      <img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.vortez.net%2Fcontentteller.php%3Fct%3Darticles%26action%3Dfile%26id%3D37007&f=1" alt="4">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+   <img class="card-img-top" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.YD0cHz0eoEwtrWLMlyrXQwHaD6%26pid%3D15.1&f=1" alt="Card image cap">
+  
   <div class="card-body">
-    <h5 class="card-title">AMD</h5>
-    <p class="card-text"> RYZEN THREADRIPPER 1950x</p>
+     <h2 class="card-title">AMD</h2> <h4>
+    <p class="card-text"> RYZEN THREADRIPPER 1950x</p> </h4>
 	 <ul class="list-group list-group-flush">
     <li class="list-group-item">Cena: 800$</li>
-  </ul>
-	 <p class="card-text">Parametry techniczne:</p>
+  </ul> <h5>
+	 <p class="card-text">Parametry techniczne:</p> </h5>
   </div>
   <ul class="list-group list-group-flush">
     <li class="list-group-item">Rdzenie: 16</li>
     <li class="list-group-item">Wątki: 32</li>
     <li class="list-group-item">Częstotliwość bazowa:3.4Ghz <br> Częstotliwość turbo:4Ghz</li>
 	<li class="list-group-item">CMOS: 14nm</li>
+	<li class="list-group-item">Odblokowany mnożnik: TAK</li>
+	<li class="list-group-item">Gniazdo procesora: sTR4</li>
 	<li class="list-group-item">Pamięć Cache L1: 1.5MB<br>Pamięć Cache L2: 8MB<br>Pamięć Cache L3: 32MB</li>
 	<li class="list-group-item">Domyślne TDP: 180W</li>
   </ul>
-  <div class="card-body">
-    <a href="https://www.amd.com/en/products/cpu/amd-ryzen-threadripper-1950x" class="card-link">Link do strony AMD</a>
+  <div class="card-body"> <h5>
+    <a href="https://www.amd.com/en/products/cpu/amd-ryzen-threadripper-1950x" class="card-link">Link do strony AMD</a> </h5>
   </div>
-</div>
+  </div>
   </body>
  </html>
