370d76e380288a9877be593605d5f231390d9f24
Paweł Porosło <pawel.poroslo@gmail.com>
2018-07-17 10:28:56 +0200

    dysk

---
+++ index2.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <title>Dysk SSD</title>
+ </head>
+ <body>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+  <nav class="navbar navbar-dark bg-light"> 
+
+   <a class="navbar-brand">Menu</a> 
+   
+    <form class="form-inline">
+     <input class="form-control mr-sm-2" type="search" placeholder="Szukaj" aria-label="Search">
+     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Szukaj</button>
+    </form>
+	
+	<button class="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+   </nav>
+ 
+   <div class="card" style="width: 24rem;">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+   <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+   <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img style=" width:500%; height: 300px !important"; class="d-block w-100" src="http://www.goodram.com/content/uploads/2017/12/ssd-IRDMPRO-03.jpg" alt="1">
+    </div>
+    <div class="carousel-item">
+      <img style=" width:500%; height: 300px !important"; class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.CqLJfC4k2BUkQPoXXMkc7AHaEc%26pid%3D15.1&f=1" alt="2">
+    </div>
+    <div class="carousel-item">
+      <img style=" width:500%; height: 300px !important"; class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.morele.net%2Fi500%2F1813250_0_i500.jpg&f=1" alt="3">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+   <img class="card-img-top" src="http://www.goodram.com/content/uploads/2017/12/ssd-IRDMPRO-01-front.jpg" alt="Card image cap">
+  
+  <div class="card-body">
+     <h2 class="card-title">GoodRAM</h2> <h4>
+    <p class="card-text">SSD IRDM PRO</p> </h4>
+	 <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cena: 1000zł</li>
+  </ul> <h5>
+	 <p class="card-text">Parametry techniczne:</p> </h5>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Pojemność: 960GB</li>
+    <li class="list-group-item">Szybkość odczytu: 560MB/s <br>Szybkość zapisu: 530MB/s</li>
+    <li class="list-group-item">Kości pamięci: Toshiba A19 MLC</li>
+	<li class="list-group-item">Pamięć podręczna: 1 GB DDR3L</li>
+	<li class="list-group-item">Pobór energji: 3,5W(Maks. praca)</li>
+	<li class="list-group-item">Kontroler: Czterordzeniowy Phison PS3110-S10</li>
+  </ul>
+  <div class="card-body"> <h5>
+    <a href="http://www.goodram.com/pl/product/produkty/ssd-irdm-pro/" class="card-link">Link do strony GoodRAM</a> <br>
+  </div>
+  </div>
+  </body>
+ </html>
