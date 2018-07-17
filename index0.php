2491541e95750e96674938746e1808548be8558b
Paweł Porosło <pawel.poroslo@gmail.com>
2018-07-17 12:33:03 +0200

    jedna

--- index0.php
+++ index0.php
@@ -104,64 +104,7 @@
   </div>
   </div></i>
           </div>
-          <div class="col-sm">
-              <i><div class="card" style="width: 22rem;">
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-   <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-	<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-  </ol>
-   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.videocardz.com%2F1%2F2017%2F07%2FGIGABYTE-RX-Vega-64-Ref.jpg&f=1" alt="1">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fimage11.macovi.de%2Fimages%2Fproduct_images%2F1280%2F1188445_1__70250-1.jpg&f=1" alt="2">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.evetech.co.za%2Frepository%2FProductImages%2Fgigabyte-radeon-rxvega64-8g-graphic-card-1000px-v1-0005.jpg&f=1" alt="3">
-    </div>
-	 <div class="carousel-item">
-      <img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.tcPaPMpIjZHcOOmtIb2vJQHaHa%26pid%3D15.1&f=1" alt="4">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-   <img class="card-img-top" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.bhphotovideo.com%2Fimages%2Fimages1000x1000%2Fgigabyte_gv_rxvega64_8gd_b_radeon_rx_vega_64_1357316.jpg&f=1" alt="Card image cap">
-  
-  <div class="card-body">
-     <h2 class="card-title">AMD / Gigabyte</h2> <h4>
-    <p class="card-text">Radeon RX Vega 64</p> </h4>
-	 <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cena: 500$</li>
-  </ul> <h5>
-	 <p class="card-text">Parametry techniczne:</p> </h5>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">procesory strumieniowe: 4096</li>
-    <li class="list-group-item">Częstotliwość bazowa: 1247Mhz <br> Częstotliwość turbo: 1546Ghz</li>
-    <li class="list-group-item">Pamięć: 8GB HBM2 2048-bit </li>
-	<li class="list-group-item">Taktowanie pamięci: 945Mhz</li>
-	<li class="list-group-item">FinFet: 14nm</li>
-	<li class="list-group-item">Domyślne TDP: 295W</li>
-	<li class="list-group-item">Wyjścia: 1*HDMI / 3*DisplayPort</li>
-  </ul>
-  <div class="card-body"> <h5>
-    <a href="https://www.amd.com/en/products/graphics/radeon-rx-vega-64" class="card-link">Link do strony AMD</a> <br>
-	<a href="https://www.gigabyte.com/Graphics-Card/GV-RXVEGA64SIL-8GD-B#kf" class="card-link">Link do strony Gigabyte</a> </h5>
-  </div>
-  </div></i>
-          </div>
+          
       </div>
   </div>
 
