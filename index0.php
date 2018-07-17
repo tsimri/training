1a503b2097a7cbded1716f717f81a323e23234b3
Paweł Porosło <pawel.poroslo@gmail.com>
2018-07-17 15:41:06 +0200

    czyszczenie z anych i obazów

--- index0.php
+++ index0.php
@@ -37,6 +37,7 @@
 								<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
 								<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
 								<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
 							</ol>
 							<div class="carousel-inner">
 								<div class="carousel-item active">
@@ -51,6 +52,9 @@
 								<div class="carousel-item">
 									<img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.vortez.net%2Fcontentteller.php%3Fct%3Darticles%26action%3Dfile%26id%3D37007&f=1" alt="3">
 								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.YD0cHz0eoEwtrWLMlyrXQwHaD6%26pid%3D15.1&f=1" alt="4">
+								</div>
 							</div>
 							<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -61,25 +65,13 @@
 								<span class="sr-only">Next</span>
 							</a>
 						</div>
-						<img class="card-img-top" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.YD0cHz0eoEwtrWLMlyrXQwHaD6%26pid%3D15.1&f=1" alt="Card image cap">
 						<div class="card-body">
 							<h2 class="card-title">AMD</h2> <h4>
 							<p class="card-text"> RYZEN THREADRIPPER 1950x</p> </h4>
 							<ul class="list-group list-group-flush">
 								<li class="list-group-item">Cena: 800$</li>
 							</ul> <h5>
-							<p class="card-text">Parametry techniczne:</p> </h5>
 						</div>
-						<ul class="list-group list-group-flush">
-							<li class="list-group-item">Rdzenie: 16</li>
-							<li class="list-group-item">Wątki: 32</li>
-							<li class="list-group-item">Częstotliwość bazowa:3.4Ghz <br> Częstotliwość turbo:4Ghz</li>
-							<li class="list-group-item">CMOS: 14nm</li>
-							<li class="list-group-item">Odblokowany mnożnik: TAK</li>
-							<li class="list-group-item">Gniazdo procesora: sTR4</li>
-							<li class="list-group-item">Pamięć Cache L1: 1.5MB<br>Pamięć Cache L2: 8MB<br>Pamięć Cache L3: 32MB</li>
-							<li class="list-group-item">Domyślne TDP: 180W</li>
-						</ul>
 						<div class="card-body"> <h5>
 							<a href="https://www.amd.com/en/products/cpu/amd-ryzen-threadripper-1950x" class="card-link">Link do strony AMD</a> </h5>
 						</div>
@@ -92,6 +84,7 @@
 								<li data-target="#carouselExampleIndicators1" data-slide-to="0" class="active"></li>
 								<li data-target="#carouselExampleIndicators1" data-slide-to="1"></li>
 								<li data-target="#carouselExampleIndicators1" data-slide-to="2"></li>
+								<li data-target="#carouselExampleIndicators1" data-slide-to="3"></li>
 							</ol>
 							<div class="carousel-inner">
 								<div class="carousel-item active">
@@ -103,6 +96,9 @@
 								<div class="carousel-item">
 									<img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.morele.net%2Fi500%2F1813250_0_i500.jpg&f=1" alt="2">
 								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="http://www.goodram.com/content/uploads/2017/12/ssd-IRDMPRO-01-front.jpg" alt="3">
+								</div>
 							</div>
 							<a class="carousel-control-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev">
 								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -113,23 +109,13 @@
 								<span class="sr-only">Next</span>
 							</a>
 						</div>
-						<img class="card-img-top" src="http://www.goodram.com/content/uploads/2017/12/ssd-IRDMPRO-01-front.jpg" alt="Card image cap">
 						<div class="card-body">
 							<h2 class="card-title">GoodRAM</h2> <h4>
 							<p class="card-text">SSD IRDM PRO</p> </h4>
 							<ul class="list-group list-group-flush">
 								<li class="list-group-item">Cena: 1000zł</li>
 							</ul> <h5>
-							<p class="card-text">Parametry techniczne:</p> </h5>
 						</div>
-						<ul class="list-group list-group-flush">
-							<li class="list-group-item">Pojemność: 960GB</li>
-							<li class="list-group-item">Szybkość odczytu: 560MB/s <br>Szybkość zapisu: 530MB/s</li>
-							<li class="list-group-item">Kości pamięci: Toshiba A19 MLC</li>
-							<li class="list-group-item">Pamięć podręczna: 1 GB DDR3L</li>
-							<li class="list-group-item">Pobór energji: 3,5W(Maks. praca)</li>
-							<li class="list-group-item">Kontroler: Czterordzeniowy Phison PS3110-S10</li>
-						</ul>
 						<div class="card-body"> <h5>
 							<a href="http://www.goodram.com/pl/product/produkty/ssd-irdm-pro/" class="card-link">Link do strony GoodRAM</a> <br>
 						</div>
@@ -143,6 +129,7 @@
 								<li data-target="#carouselExampleIndicators2" data-slide-to="1"></li>
 								<li data-target="#carouselExampleIndicators2" data-slide-to="2"></li>
 								<li data-target="#carouselExampleIndicators2" data-slide-to="3"></li>
+								<li data-target="#carouselExampleIndicators2" data-slide-to="4"></li>
 							</ol>
 							<div class="carousel-inner">
 								<div class="carousel-item active">
@@ -157,6 +144,9 @@
 								<div class="carousel-item">
 									<img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.tcPaPMpIjZHcOOmtIb2vJQHaHa%26pid%3D15.1&f=1" alt="3">
 								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.bhphotovideo.com%2Fimages%2Fimages1000x1000%2Fgigabyte_gv_rxvega64_8gd_b_radeon_rx_vega_64_1357316.jpg&f=1" alt="4">
+								</div>
 							</div>
 							<a class="carousel-control-prev" href="#carouselExampleIndicators2" role="button" data-slide="prev">
 								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -167,24 +157,13 @@
 								<span class="sr-only">Next</span>
 							</a>
 						</div>
-						<img class="card-img-top" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.bhphotovideo.com%2Fimages%2Fimages1000x1000%2Fgigabyte_gv_rxvega64_8gd_b_radeon_rx_vega_64_1357316.jpg&f=1" alt="Card image cap">
 						<div class="card-body">
 							<h2 class="card-title">AMD / Gigabyte</h2> <h4>
 							<p class="card-text">Radeon RX Vega 64</p> </h4>
 							<ul class="list-group list-group-flush">
 								<li class="list-group-item">Cena: 500$</li>
 							</ul> <h5>
-							<p class="card-text">Parametry techniczne:</p> </h5>
 						</div>
-						<ul class="list-group list-group-flush">
-							<li class="list-group-item">procesory strumieniowe: 4096</li>
-							<li class="list-group-item">Częstotliwość bazowa: 1247Mhz <br> Częstotliwość turbo: 1546Ghz</li>
-							<li class="list-group-item">Pamięć: 8GB HBM2 2048-bit </li>
-							<li class="list-group-item">Taktowanie pamięci: 945Mhz</li>
-							<li class="list-group-item">FinFet: 14nm</li>
-							<li class="list-group-item">Domyślne TDP: 295W</li>
-							<li class="list-group-item">Wyjścia: 1*HDMI / 3*DisplayPort</li>
-						</ul>
 						<div class="card-body"> <h5>
 							<a href="https://www.amd.com/en/products/graphics/radeon-rx-vega-64" class="card-link">Link do strony AMD</a> <br>
 							<a href="https://www.gigabyte.com/Graphics-Card/GV-RXVEGA64SIL-8GD-B#kf" class="card-link">Link do strony Gigabyte</a> </h5>
