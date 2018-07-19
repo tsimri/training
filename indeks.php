d1c943fcd5e4232ebd7b0b332562dafdf2c0635e
Paweł Porosło <pawel.poroslo@gmail.com>
2018-07-19 15:12:03 +0200

    poprawka

--- indeks.php
+++ indeks.php
@@ -17,27 +17,29 @@
 			$result = mysqli_query ($link,$q);
 		?>
 		<div class="container mt-5 mb-5">
-		<div class="row mt-3 mb-3">
-		<div class="col-lg text-left">
+			<div class="row mt-3 mb-3">
+				<div class="col-lg text-left">
 
-		<?php	
-			while ($product = mysqli_fetch_assoc ($result)) {    // w tym miejscu mamy tak jakby pierwszą kolumne tablie
-		?>     <!-- W tym miejscu konczym skrytp PHP a zaczynaymy HTML -->
-                    <div class="card-body">
-					<h1 style="color:red"> <img src= <?php echo $product['Zdjęcie']?>></h1>
-                        <h2 class="card-title">Producent: <?php echo $product['Producent']?></h2>
-                            <h4>	
-                                <p class="card-text">Nazwa: <?php echo $product['Nazwa']?></p>
-                            </h4>
-                            <h1 class="card-text" style="color:red">Cena: <?php echo $product['Cena']?></h1>
+					<?php	
+						while ($product = mysqli_fetch_assoc ($result)) {    // w tym miejscu mamy tak jakby pierwszą kolumne tablie
+					?>     <!-- W tym miejscu konczym skrytp PHP a zaczynaymy HTML -->
+					<div class="cool-sm">
+						<div class="card-body">
+							<h1 style="color:red" class="card-img-top">  <img src= <?php echo $product['Zdjęcie']?>></h1>
+							<h2 class="card-title">Producent: <?php echo $product['Producent']?></h2>
+							<h4>	
+								<p class="card-text">Nazwa: <?php echo $product['Nazwa']?></p>
+							</h4>
+							<h1 class="card-text" style="color:red">Cena: <?php echo $product['Cena']?></h1>
 							<h6 class="card-text" style="color:green">Ilość sztuk: <?php echo $product['Ilość']?></h6>
-                    </div>
+						</div>
+					</div>
+					<?php
+						}
+					?>
 				</div>
 			</div>
-        <?php
-
-        }
-			?>
+		</div>
 		<?php
 			/*$q = mysqli_query($link,$q);
 			echo '<ul>';
