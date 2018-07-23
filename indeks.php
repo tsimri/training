80c24ebe9acd0d96b707ae4c23b700781f50fcf6
Paweł Porosło <pawel.poroslo@gmail.com>
2018-07-23 15:08:39 +0200

    Linki

--- indeks.php
+++ indeks.php
@@ -31,7 +31,7 @@
 								</h4>
 								<h1 class="card-text" style="color:red">Cena: <?php echo $product['Cena']?></h1>
 								<h6 class="card-text" style="color:green">Ilość sztuk: <?php echo $product['Ilość']?></h6>
-								<h5><a href="<?php echo $Link['Link']?>" target="_blank" class="card-link">Link do szczegółów.</a></h5>
+								<h5><a href="http://localhost/detail.php?id<?php echo $product['id']?>">Link do szczegółów.</a></h5>
 							</div>	
 						</div>
 					</div>
