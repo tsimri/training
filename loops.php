06cc5749cd94eb5d1593d7a23aee7eb901a75dd1
Paweł Porosło <pawel.poroslo@gmail.com>
2018-07-26 12:50:18 +0200

    pętla

--- loops.php
+++ loops.php
@@ -7,9 +7,9 @@ while($zmienna < 11)
   echo $zmienna; 
   $zmienna++;
 
-if ($zmienna %3===0)  ?><br><?php
+if ($zmienna %3===0) 
 {   
-	echo "Podzielna przez 3"; ?><br><?php
+	echo "Podzielna przez 3 </br>"; 
 }} 
  echo $zmienna ;  ?><br><?php
  echo 0/3  ?><br><?php ;echo 1/3 ?><br><?php ; echo 3/3 ?><br><?php;  ?><br><?php?><br><?php?><br><?php?><br><?php
