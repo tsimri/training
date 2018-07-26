36c757ba7399602a039e51c3953dd07bed27adec
ainf024 <ainf024@ab.pl>
2018-07-26 12:54:09 +0200

    Beautifull code

--- loops.php
+++ loops.php
@@ -1,17 +1,24 @@
 <html>
-<body>
-<?php
- $zmienna = 0;
-while($zmienna < 11) 
-{
-  echo $zmienna; 
-  $zmienna++;
+    <body>
 
-if ($zmienna %3===0) 
-{   
-	echo "Podzielna przez 3 </br>"; 
-}} 
- echo $zmienna ;  ?><br><?php
- echo 0/3  ?><br><?php ;echo 1/3 ?><br><?php ; echo 3/3 ?><br><?php;  ?><br><?php?><br><?php?><br><?php?><br><?php
- echo 0%3 ?><br><?php ;echo 1%3; ?><br><?php ;echo 3%3 ?><br><?php
+<?php
+    $zmienna = 0;
+    while($zmienna < 11) {
+        echo $zmienna;
+        $zmienna++;
+        if ($zmienna %3===0) {
+            echo "</br>Podzielna przez 3</br>";
+        }
+    }
+    echo $zmienna . "</br>";
+    echo 0/3  . "</br>";
+    echo 1/3 . "</br>";
+    echo 3/3 . "</br></br></br>";
+
+    echo 0%3 . "</br>";
+    echo 1%3 . "</br>";
+    echo 3%3 . "</br>";
 ?>
+
+    </body>
+</html>
