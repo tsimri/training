5d08a31a26f60d96f17acaa50b677d761b546667
Paweł Porosło <pawel.poroslo@gmail.com>
2018-08-24 14:03:28 +0200

    dodanie modala

--- indeks.php
+++ indeks.php
@@ -104,7 +104,7 @@
 				<div class="modal-dialog" role="document">
 					<div class="modal-content">
 						<div class="modal-header">
-							<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+							<h5 class="modal-title" id="exampleModalLabel">Okno</h5>
 							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 								<span aria-hidden="true">&times;</span>
 							</button>
@@ -113,8 +113,8 @@
 							...
 						</div>
 						<div class="modal-footer">
-							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-							<button type="button" class="btn btn-primary">Save changes</button>
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
+							<button type="button" class="btn btn-primary">Zapisz zmiany</button>
 						</div>
 					</div>
 				</div>
